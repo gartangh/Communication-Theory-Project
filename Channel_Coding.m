2fef703dd4b774e9db7b217a549b26dfb28acab4
tmuytten <thomas.uyttenhove@ugent.be>
2017-11-26 18:48:32 +0100

    Channel_Coding.Encode_outer en .Decode_outer done

--- Channel_Coding.m
+++ Channel_Coding.m
@@ -18,17 +18,27 @@ classdef Channel_Coding
                  [0 0 0 0 0 1 0 0 1 0 0 0 1 0];
                  [0 1 0 0 0 1 1 1 1 0 0 0 0 0];
                  [0 1 0 0 0 0 1 0 0 0 0 0 0 1];
-                 [0 0 0 0 0 0 1 0 1 1 0 0 0 0];] % vul hier de generatormatrix in
+                 [0 0 0 0 0 0 1 0 1 1 0 0 0 0];]; % vul hier de generatormatrix in
             
-             % Minimale Hamming afstand: Via H^T; minimale set van rijen die
-            % de nul-vector uitkomt (c*H^T = 0), waarbij Hamming gewicht =
-            % Hamming afstand = aantal enen die set voorstelt
+            % Minimale Hamming afstand = 3:
+            % Via H^T; minimale set van rijen die de nul-vector uitkomt (c*H^T = 0),
+            % waarbij Hamming gewicht = Hamming afstand = aantal enen die set voorstelt
             
+            % gegarandeerd foutcorrigerend vermogen t = 1 = floor((d_min - 1)/2)
+            % gegarandeerd foutdetecterend vermogen £ = 2 = d_min - 1
             
             bitstring = bitstring(:)'; % bitstring zeker een rij vector 
-            N = length(bitstring);
+            %bitstring = str2num(bitstring); % bitstring omzetten naar vector
+            N = length(bitstring); 
             N_codewords = ceil(N/10);
-            bitstring = [bitstring zeros(1, N_codewords*10-N)]; % vul aan met nullen als bitstring geen geheel aantal informatiewoorden is. 
+            bitstring = [bitstring, zeros(1, N_codewords*10-N)]; % vul aan met nullen als bitstring geen geheel aantal informatiewoorden is.             
+            
+            bitenc = zeros(1,N_codewords*14);
+    
+            for i = 1:N_codewords
+                b = bitstring(((i-1)*10+1):i*10);
+                bitenc(1, ((i-1)*14+1):i*14) = mod(b * G,2);
+            end
         end
         
         % Functie die de decoder van de outer code implementeert
@@ -43,10 +53,23 @@ classdef Channel_Coding
                  [0 0 1 1 0 0 1 1 0 1 0 1 0 1];
                  [1 0 0 0 0 0 0 1 1 1 1 1 1 0]]; % checkmatrix
             
+            H_T = H.'; % getransponeerde checkmatrix
+            S = syndtable(H); % syndroomtabel
+           
             bitstring = bitstring(:)';
             N = length(bitstring);
             N_codewords = ceil(N/14);
-                        
+            
+            bool_error = zeros(1, N_codewords * 14);
+            
+            for i = 1:N_codewords
+                r = bitstring(((i-1)*14+1):i*14);
+                s = mod(r * H_T, 2); % syndroom
+                bool_error(((i-1)*14+1):i*14) = S(bi2de(fliplr(s)) + 1, :); % geschatte foutvector
+            end
+            
+            bitsdec = mod(bitstring + bool_error,2);
+            
         end
         
         % Functie die de encoder van de inner code implementeert
