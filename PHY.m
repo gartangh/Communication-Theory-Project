f871427ebcbc46e5735a98118deb6f562630238c
Garben Tanghe <garben.tanghe@gmail.com>
2017-12-02 17:22:48 +0100

    zoveel mogelijk verder gewerkt aan te implimenteren functies

--- PHY.m
+++ PHY.m
@@ -1,8 +1,7 @@
 classdef PHY
           
    methods(Static=true)
-        
-             
+         
         % Functie die een bitstring omzet naar complexe symbolen.
         function a = mapper(bitstring, constellation)
             % INPUT
@@ -10,8 +9,7 @@ classdef PHY
              % constellation : ofwel 'BPSK', '4QAM', '4PSK', '4PAM', etc.
             % OUTPUT
              % a : vector met (complexe) symbolen. 
-            
-                       
+                
             switch(constellation)
                 case 'BPSK'                    
                     % BPSK mapping here.
@@ -29,13 +27,13 @@ classdef PHY
                         y = bitstring(2*j);
                         
                         if (x==0 && y==0)
-                            a(j) = sqrt(2)*exp(1*1i*pi/4);
+                            a(j) = sqrt(2)*deltaQAM*exp(1*1i*pi/4);
                         elseif (x==0 && y==1)
-                            a(j) = sqrt(2)*exp(3*1i*pi/4);
-                        elseif (x==1 && y==0)
-                            a(j) = sqrt(2)*exp(5*1i*pi/4);
-                        elseif(x==1 && y==1)
-                            a(j) = sqrt(2)*exp(7*1i*pi/4);
+                            a(j) = sqrt(2)*deltaQAM*exp(3*1i*pi/4);
+                        elseif (x==1 && y==1)
+                            a(j) = sqrt(2)*deltaQAM*exp(5*1i*pi/4);
+                        elseif(x==1 && y==0)
+                            a(j) = sqrt(2)*deltaQAM*exp(7*1i*pi/4);
                         else
                             % Incorrect input
                         end
@@ -56,9 +54,9 @@ classdef PHY
                             a(j) = -3*deltaPAM;
                         elseif (x==0 && y==1)
                             a(j) = -1*deltaPAM;
-                        elseif (x==1 && y==0)
+                        elseif (x==1 && y==1)
                             a(j) = 1*deltaPAM;
-                        elseif(x==1 && y==1)
+                        elseif(x==1 && y==0)
                             a(j) = 3*deltaPAM;
                         else
                             % Incorrect input
@@ -67,8 +65,7 @@ classdef PHY
                                
                 otherwise
                     error('Constellation not recognized');
-            end
-                        
+            end    
         end
         
         % Functie die complexe symbolen omzet naar bits.
@@ -92,18 +89,18 @@ classdef PHY
                     bitstring = zeros(length(a)*2);
                     
                     for j = 1:length(a)
-                        if (a(j) == sqrt(2)*exp(1*1i*pi/4))
+                        if      (a(j) == sqrt(2)*deltaQAM*exp(1*1i*pi/4))
                             bitstring(2*j-1)    = 0;
                             bitstring(2*j)      = 0;
-                        elseif (a(j) == sqrt(2)*exp(3*1i*pi/4))
+                        elseif  (a(j) == sqrt(2)*deltaQAM*exp(3*1i*pi/4))
                             bitstring(2*j-1)    = 0;
                             bitstring(2*j)      = 1;
-                        elseif (a(j) == sqrt(2)*exp(5*1i*pi/4))
-                            bitstring(2*j-1)    = 1;
-                            bitstring(2*j)      = 0;
-                        elseif (a(j) == sqrt(2)*exp(7*1i*pi/4))
+                        elseif  (a(j) == sqrt(2)*deltaQAM*exp(5*1i*pi/4))
                             bitstring(2*j-1)    = 1;
                             bitstring(2*j)      = 1;
+                        elseif  (a(j) == sqrt(2)*deltaQAM*exp(7*1i*pi/4))
+                            bitstring(2*j-1)    = 1;
+                            bitstring(2*j)      = 0;
                         else
                             % Incorrect input
                         end
@@ -125,10 +122,10 @@ classdef PHY
                             bitstring(2*j)      = 1;
                         elseif (a(j) == 1*deltaPAM)
                             bitstring(2*j-1)    = 1;
-                            bitstring(2*j)      = 0;
+                            bitstring(2*j)      = 1;
                         elseif (a(j) == 3*deltaPAM)
                             bitstring(2*j-1)    = 1;
-                            bitstring(2*j)      = 1;
+                            bitstring(2*j)      = 0;
                         else
                             % Incorrect input
                         end
@@ -147,16 +144,52 @@ classdef PHY
             % OUTPUT
              % a_estim : vector met geschatte (complexe) symbolen.           
             
+            a_estim = zeros(length(x)); 
+            
             switch(constellation)
                 case 'BPSK'                   
-                    % BPSK here      
+                    % BPSK here
+                    for i = 1:length(x)
+                        if real(x(i)) < 0
+                            a_estim(i) = -1;
+                        else
+                            a_estim(i) = 1;
+                        end
+                    end
                     
+                case '4QAM'
+                    % 4QAM here            
+                    for i = 1:length(x)
+                        if (real(x(i)) >= 0 && imag(x(i)) >= 0)
+                            a_estim(i) = sqrt(2)*deltaQAM*exp(1*1i*pi/4);
+                        elseif (real(x(i)) < 0 && imag(x(i)) > 0)
+                            a_estim(i) = sqrt(2)*deltaQAM*exp(3*1i*pi/4);
+                        elseif (real(x(i)) <= 0 && imag(x(i)) <= 0)
+                            a_estim(i) = sqrt(2)*deltaQAM*exp(5*1i*pi/4);
+                        elseif (real(x(i)) > 0 && imag(x(i)) < 0)
+                            a_estim(i) = sqrt(2)*deltaQAM*exp(7*1i*pi/4);
+                        else
+                            % Incorrect input
+                        end
+                    end
+                    
+                case '4PAM'
+                    % 4PAM here
+                    dPAM = sqrt(12/(4^2-1));
+                    deltaPAM = dPAM/2;
+                    
+                    for i = 1:length(x)
+                        a_estim(i) = round(real(x(i))/deltaPAM/2) * 2;
+                        if (a_estim(i) >= 0)
+                            a_estim(i) = a_estim(1) + 1;
+                        else
+                            a_estim(i) = a_estim(1) - 1;
+                        end
+                    end
+                
                 otherwise
-                    error('Constellation not recognized');
-                    
+                    error('Constellation not recognized');  
             end
-                
-                
         end
         
         % Functie die de decisie-variabele afleidt uit rdown.
@@ -168,6 +201,9 @@ classdef PHY
             % OUTPUT
              % u : vector met decisie-variabele.
             
+            u = rdown/hch_hat*exp(1i*theta_hat);
+            
+            scatter(real(u),imag(u));   
         end
         
         % Functie die symbolen op een puls zet en dit signaal moduleert op een golf.
@@ -186,19 +222,17 @@ classdef PHY
              % De bemonsteringsfrequentie Ns/T moet minstens tweemaal zo hoog zijn
              % als de hoogste frequentie, aanwezig in het signaal,
              % om het origineel zonder fouten te kunnen reproduceren.
-            tijdsspan = -Lf*T:T/Ns:Lf*T;
-            x = zeros(length(a)*length(t));
             
-            for i = 1:length(a)
-                % optelling uit opgave (3) hier gebruiken!
-                y = a(i)*pulse(tijdsspan,T,alpha);
-                x((i-1)*length(y)+1:i*length(y)) = y;
+            t = -Lf*T:T/Ns:Lf*T;
+            p = pulse(t,T,alpha);
+            % optelling uit opgave (2)
+            x = zeros(length(t),1);
+            for k = 0:length(a)-1
+                x(t/T*Ns+Lf*T) = x(t/T*Ns+Lf*T) + a(k)*p(t/T*Ns+Lf*T - k*T/T*Ns+Lf*T);
             end
             
             % Basisbandkanaal op draaggolf met freqentie frequency
-            s = zeros(lenght(x));
-            s(i) = sqrt(2)*real(x(i)*exp(1i*2*pi*frequency*i*T/length(t)));
-            
+            s(t/T*Ns+Lf*T) = sqrt(2)*real(x(t/T*Ns+Lf*T)*exp(1i*2*pi*frequency*t*T)); 
         end
         
         function rdemod = demodulate(r,T,Ns,frequency,alpha,Lf,theta)
@@ -212,7 +246,13 @@ classdef PHY
              % Lf : pulse duur (in aantal symboolperioden).
             % OUTPUT
              % rdemod: vector met gedemoduleerde samples.
-                        
+            
+            t = -Lf*T:T/Ns:Lf*T;
+            p = pulse(t,T,alpha);
+            
+            r(t/T*Ns+Lf*T) = sqrt(2)*r(t/T*Ns+Lf*T)*exp(-1i*(2*pi*frequency*t*Ns/T + theta));
+            
+            rdemod = T/Ns * conv(r,p);
         end
         
         function y = pulse(t,T,alpha)
@@ -238,10 +278,9 @@ classdef PHY
             twee = (alpha)*cos(pi*(t/T-0.25)).*sinc(alpha*t/T-0.25);
             drie = (alpha)*cos(pi*(t/T+0.25)).*sinc(alpha*t/T+0.25);
             y    = 1/(sqrt(T)) * (een + twee + drie);
-
         end
         
-        % Functie die het gegeven signaal decimeert met factor Ns en het overgansverschijnsel verwijdert
+        % Functie die het gegeven signaal decimeert met factor Ns en het overgangsverschijnsel verwijdert
         function rdown=downsample(rdemod,Ns,Lf)
             % INPUT
              % rdemod : vector met Ns samples per symbool.
@@ -249,6 +288,12 @@ classdef PHY
              % Lf : pulse duration (in number of symbol periods)
             % OUTPUT
              % rdown: vector met 1 sample per symbool
+            
+            % Decimeren met factor Ns
+            rdown = decimate(rdemod, Ns);
+            
+            % Overgangsverschijnsel verwijderen
+            rdown = rdown; % Hier nog formule op toepassen!
         end   
         
         % Functie die het kanaal simuleert
@@ -260,8 +305,8 @@ classdef PHY
             % OUTPUT
              % r : rijvector met samples van de uitgang van het kanaal.
             
+            nl = sqrt(sigma) * (1/sqrt(2) * (randn(1,lenght(s)) + 1i * randn(1,lenght(s))));
+            r = hch*s + nl;
         end
-            
-    end
-    
+   end
 end
